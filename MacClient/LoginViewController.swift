8f43b5322cd08114534d6ae37b1d0f49c482dd4e
Mark Lilback <mark@lilback.com>
2016-01-18 05:58:38 -0500

    implemented basic login window

--- MacClient/LoginViewController.swift
+++ MacClient/LoginViewController.swift
@@ -7,19 +7,62 @@
 import Cocoa
 
 class LoginViewController: NSViewController {
-
+	///key for bool value in NSUserDefaults
+	let AutoLoginPrefKey = "AutomaticallyLogin"
+	
+	///The user specified login name
+	var loginName : String?
+	///The user specified password
+	var password : String?
+	///Value is save/restored to NSUserDefaults
+	var autoSignIn : Bool = false
+	///When true, controls are disabled and spinning progress indicator is enabled
+	var isBusy : Bool = false
+	///Closure called when the user presses the login button
+	var completionHandler : ((controller: LoginViewController) -> Void)?
+	///An array of host names to display to the user
+	var hosts : [String] = ["localhost"] {
+		didSet { selectedHost = hosts.first; arrayController?.content = hosts }
+	}
+	///The user selected host. Defaults to first host in hosts array
+	var selectedHost : String?
+	
+	@IBOutlet var arrayController: NSArrayController!
+	
+	//TODO: load login/password from keychain
+	//TODO: autologin if have data from keychain
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
-		// Do any additional setup after loading the view.
-	}
-
-	override var representedObject: AnyObject? {
-		didSet {
-		// Update the view, if already loaded.
+		if NSUserDefaults.standardUserDefaults().boolForKey(AutoLoginPrefKey) {
+			autoSignIn = true
 		}
 	}
 
-
+	override func viewWillDisappear() {
+		super.viewWillDisappear()
+		NSUserDefaults.standardUserDefaults().setBool(autoSignIn, forKey: AutoLoginPrefKey)
+	}
+	
+	/** 
+		Owner should call this when the completionHandler has finished the async login attempt.
+		This will set isBusy to false and show any error message.
+		
+		- Parameter error: nil if successful, a message to display if failed
+	*/
+	func loginAttemptComplete(error: String? = nil) {
+		isBusy = false
+		if error != nil {
+			//TODO: show error message
+			log.error(error)
+		} else {
+			self.view.window?.orderOut(self)
+			password = nil
+		}
+	}
+	
+	/// action used by the login button
+	@IBAction func attemptLogin(sender:AnyObject) {
+		completionHandler!(controller: self)
+	}
 }
 
--- MacClient/MacAppDelegate.swift
+++ MacClient/MacAppDelegate.swift
@@ -11,19 +11,42 @@ let log = XCGLogger.defaultInstance()
 
 @NSApplicationMain
 class MacAppDelegate: NSObject, NSApplicationDelegate {
-
+	var loginWindowController: NSWindowController?
+	var loginController: LoginViewController?
+	
 	func applicationWillFinishLaunching(notification: NSNotification) {
 		log.setup(.Debug, showLogIdentifier: false, showFunctionName: true, showThreadName: false, showLogLevel: true, showFileNames: true, showLineNumbers: true, showDate: false, writeToFile: nil, fileLogLevel: .Debug)
 	}
 
 	func applicationDidFinishLaunching(aNotification: NSNotification) {
-		// Insert code here to initialize your application
+		let sboard = NSStoryboard(name: "Main", bundle: nil)
+		loginWindowController = sboard.instantiateControllerWithIdentifier("loginWindow") as? NSWindowController
+		loginController = loginWindowController?.window?.contentViewController as? LoginViewController
+		showLoginWindow()
 	}
 
 	func applicationWillTerminate(aNotification: NSNotification) {
 		// Insert code here to tear down your application
 	}
 
-
+	func applicationShouldOpenUntitledFile(sender: NSApplication) -> Bool {
+		return NSApp.modalWindow == nil
+	}
+	
+	func applicationOpenUntitledFile(sender: NSApplication) -> Bool {
+		showLoginWindow()
+		return true
+	}
+	
+	func attemptLogin(controller: LoginViewController) {
+		NSApp.stopModal()
+		loginController!.loginAttemptComplete(nil)
+	}
+	
+	func showLoginWindow() {
+		loginController!.hosts = RestServer.sharedInstance.restHosts
+		loginController!.completionHandler = attemptLogin
+		NSApp!.runModalForWindow((loginWindowController?.window)!)
+	}
 }
 
