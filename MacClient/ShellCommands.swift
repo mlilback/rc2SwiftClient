7caf89dd657ce4d3aea2c588c75a305a3ecee648
Mark Lilback <mark@lilback.com>
2016-06-23 13:33:54 -0400

    added class to execute shell command and return stdout

---
+++ MacClient/ShellCommands.swift
@@ -0,0 +1,44 @@
+//
+//  ShellCommands.swift
+//  SwiftClient
+//
+//  Created by Mark Lilback on 6/22/16.
+//  Copyright © 2016 Rc2. All rights reserved.
+//
+
+import Cocoa
+
+///Provides wrappers for executing shell commands
+public class ShellCommands {
+	///executes a command returning stdout, optionally matching a capture of a regular expression
+	/// - parameter for: program/script to execute
+	/// - parameter arguments: array of arguments to the command. Defaults to an empty array.
+	/// - parameter pattern: an optional regular expression to use to extract a value from stdout.
+	/// - parameter matchNumber: the index of the grouping of pattern to return. Defaults to the entire match.
+	/// - returns: stdout or the substring of stdout matching grouping matchNumber of pattern
+	static func stdout(for command:String, arguments:[String] = [], pattern:String? = nil, matchNumber:Int=0) throws -> String?
+	{
+		let task:NSTask = NSTask()
+		let pipe:NSPipe = NSPipe()
+		task.launchPath = command
+		task.arguments = arguments
+		task.standardOutput = pipe
+		task.launch()
+		
+		let handle = pipe.fileHandleForReading
+		let data = handle.readDataToEndOfFile()
+		let fullString = NSString(data: data, encoding: NSUTF8StringEncoding)!
+		var matchStr:String? = nil
+		if let regexStr = pattern
+		{
+			let regex = try NSRegularExpression(pattern: regexStr, options: [])
+			if let matchResult = regex.firstMatchInString(fullString as String, options: [], range: NSMakeRange(0, fullString.length)) where matchResult.numberOfRanges > matchNumber
+			{
+				matchStr = fullString.substringWithRange(matchResult.rangeAtIndex(matchNumber
+					))
+			}
+		}
+		return matchStr
+	}
+	
+}
