a621174402d409c4f88d4ddeae07bd3457f300f7
Mark Lilback <mark@lilback.com>
2016-07-13 10:22:49 -0400

    moved NSError extension to own file. Updated to try looking up a description via NSLocalizedString

---
+++ shared/NSError+Rc2.swift
@@ -0,0 +1,26 @@
+//
+//  NSError+Rc2.swift
+//
+//  Copyright ©2016 Mark Lilback. This file is licensed under the ISC license.
+//
+
+import Foundation
+
+extension NSError {
+	///returns a NSError with the domain of Rc2ErrorDomain
+	/// - parameter withCode: the Rc2 error code for this error
+	/// - parameter description: if nil, will be looked up with NSLocalizedString using Rc2ErrorCode.[code]
+	/// - returns: the new error object
+	static func error(withCode code:Rc2ErrorCode, description:String?) -> NSError {
+		var userInfo:[String:AnyObject]?
+		var localDescription = description
+		if localDescription == nil {
+			localDescription = NSLocalizedString("Rc2ErrorCode.\(code)", comment: "")
+		}
+		if let desc = localDescription {
+			userInfo = [NSLocalizedDescriptionKey:desc]
+		}
+		return NSError(domain: Rc2ErrorDomain, code: code.rawValue, userInfo: userInfo)
+	}
+}
+
