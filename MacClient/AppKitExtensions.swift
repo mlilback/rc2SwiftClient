08c9ef8c22705fdb19edfa5f84c78b327ffd6adb
Mark Lilback <mark@lilback.com>
2017-02-21 17:12:14 -0500

    added NSFont method to compute font that fits in a particular width

--- MacClient/AppKitExtensions.swift
+++ MacClient/AppKitExtensions.swift
@@ -7,9 +7,33 @@
 import Cocoa
 
 extension NSFontDescriptor {
-	var fontName:String { return object(forKey: NSFontNameAttribute) as? String ?? "Unknown" }
-	var visibleName:String { return object(forKey: NSFontVisibleNameAttribute) as? String ?? fontName }
+	var fontName: String { return object(forKey: NSFontNameAttribute) as? String ?? "Unknown" }
+	var visibleName: String { return object(forKey: NSFontVisibleNameAttribute) as? String ?? fontName }
 }
+
+extension NSFont {
+	/// Returns a font with the size adjusted to fit string in maxWidth pixels, limited to minFontSize. The font will never be larger than minFontSize
+	///
+	/// - Parameters:
+	///   - string: the string to base the font size on
+	///   - maxWidth: the maximum width of the display rect for string
+	///   - minFontSize: the minimum font size, defaults to 9.0.
+	/// - Returns: an adjusted font
+	func font(for string: String, maxWidth: CGFloat, minFontSize: CGFloat = 9.0) -> NSFont {
+		let increment: CGFloat = 0.2
+		var curSize: CGFloat = 14.0
+		var sz = string.size(withAttributes: [NSFontAttributeName: self])
+		if sz.width < maxWidth { return self }
+		var curFont = self
+		while sz.width > maxWidth && (curSize - increment) > minFontSize {
+			curSize -= increment
+			curFont = NSFont(name: fontName, size: curSize)!
+			sz = string.size(withAttributes: [NSFontAttributeName: curFont])
+		}
+		return curFont
+	}
+}
+
 
 extension NSMenu {
 	func itemWithAction(_ action:Selector, recursive:Bool = true) -> NSMenuItem? {
