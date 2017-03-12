ef0fb070f4273f22de27a54eda838bf0fc3a2741
Mark Lilback <mark@lilback.com>
2017-03-12 18:13:44 -0400

    added class to monitor a folder for changes

---
+++ ClientCore/DirectoryWatcher.swift
@@ -0,0 +1,52 @@
+//
+//  DirectoryWatcher.swift
+//
+//  Copyright ©2016 Mark Lilback. This file is licensed under the ISC license.
+//
+
+import Foundation
+
+/// object that watches a directory for changes
+public final class DirectoryWatcher {
+	/// Closure called when the diretory changes
+	var changeCallback: (DirectoryWatcher) -> Void
+	
+	/// initialize a DirectoryWatcher
+	///
+	/// - Parameters:
+	///   - url: the directory to watch
+	///   - callback: closure to call when a change is detected
+	public init(url: URL, callback: @escaping (DirectoryWatcher) -> Void) {
+		self.url = url
+		changeCallback = callback
+	}
+	
+	private var monitorFileDescriptor: CInt = -1
+	private let monitorQueue = DispatchQueue(label: "io.rc2.diretoryWatcher")
+	private var monitorSource: DispatchSourceFileSystemObject?
+	let url: URL
+	
+	/// start watching the directory. has no effect if already started
+	public func start() {
+		guard monitorSource == nil else { return }
+		
+		monitorFileDescriptor = open(url.path, O_EVTONLY)
+		monitorSource = DispatchSource.makeFileSystemObjectSource(fileDescriptor: monitorFileDescriptor, eventMask: [.write, .delete], queue: monitorQueue)
+		monitorSource?.setEventHandler { [weak self] _ in
+			self?.changeCallback(self!)
+		}
+		monitorSource?.setCancelHandler { [weak self] in
+			guard let me = self else { return }
+			close(me.monitorFileDescriptor)
+			me.monitorFileDescriptor = -1
+			me.monitorSource = nil
+		}
+		monitorSource?.resume()
+	}
+	
+	/// stops/removes the directory watcher
+	public func stop() {
+		monitorSource?.cancel()
+		monitorSource = nil
+	}
+}
