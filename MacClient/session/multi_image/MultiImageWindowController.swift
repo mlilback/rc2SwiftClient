fa69007fce980d1e272f55e414d966467c178e2d
Mark Lilback <mark@lilback.com>
2017-04-04 14:04:44 -0400

    changed welcome close button to be an x instead of button

---
+++ MacClient/session/multi_image/MultiImageWindowController.swift
@@ -0,0 +1,11 @@
+//
+//  MultiImageWIndowController.swift
+//
+//  Copyright ©2016 Mark Lilback. This file is licensed under the ISC license.
+//
+
+import Cocoa
+
+class MultiImageWindowController: NSWindowController {
+	
+}
