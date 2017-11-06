5f3584775a11642f261a0df19e254a0f89741677
Mark Lilback <mark@lilback.com>
2017-11-06 13:02:41 -0500

    updated logs to hackishly just show app server logs. need to compute to use full log facilities. updated project file with Xcode 9.1

--- MacClient/docker/DockerLogController.swift
+++ MacClient/docker/DockerLogController.swift
@@ -29,34 +29,43 @@ public class DockerLogController: DockerManagerInjectable {
 	public override func viewDidLoad() {
 		super.viewDidLoad()
 		LogType.allValues.forEach { _ in logs.append(NSMutableAttributedString()) }
+		let container = manager!.containers[0]
 		DispatchQueue.main.async {
 			guard let manager = self.manager else { fatalError() }
-			LogType.allValues.forEach { logtype in
-				manager.api.streamLog(container: manager.containers[logtype.containerType]!) { [weak self] (str, _) in
-					DispatchQueue.main.async {
-						guard let me = self, let str = str else { return } //TODO: handle close
-						let attrStr = NSAttributedString(string: str)
-						me.logs[logtype.rawValue].append(attrStr)
-						if me.selectedLog == logtype {
-							// figure out if current scrolled to end of document
-							let atEnd = me.logView.enclosingScrollView?.contentView.bounds.maxY ?? 0 == me.logView.enclosingScrollView?.documentView?.bounds.height ?? 0
-							me.logView.textStorage?.append(attrStr)
-							// if was scrolled to end of document, scroll to show newly appended content
-							if atEnd { me.logView.scrollToEndOfDocument(nil) }
-						}
-					}
+			let producer = manager.api.stream(command: ["tail", "-n", "+0", "-f", "/tmp/appserver.stderr.log"], container: container)
+			producer.start { [weak self] event in
+				guard case let .value(data) = event else { return }
+				DispatchQueue.main.async {
+					guard let me = self, let str = String(data: data, encoding: .utf8) else { return }
+					me.logView.textStorage!.append(NSAttributedString(string: str))
 				}
 			}
+//			LogType.allValues.forEach { logtype in
+//				manager.api.streamLog(container: manager.containers[logtype.containerType]!) { [weak self] (str, _) in
+//					DispatchQueue.main.async {
+//						guard let me = self, let str = str else { return } //TODO: handle close
+//						let attrStr = NSAttributedString(string: str)
+//						me.logs[logtype.rawValue].append(attrStr)
+//						if me.selectedLog == logtype {
+//							// figure out if current scrolled to end of document
+//							let atEnd = me.logView.enclosingScrollView?.contentView.bounds.maxY ?? 0 == me.logView.enclosingScrollView?.documentView?.bounds.height ?? 0
+//							me.logView.textStorage?.append(attrStr)
+//							// if was scrolled to end of document, scroll to show newly appended content
+//							if atEnd { me.logView.scrollToEndOfDocument(nil) }
+//						}
+//					}
+//				}
+//			}
 		}
 		logView.textStorage?.append(logs[selectedLog.rawValue])
 		logView.scrollToEndOfDocument(nil)
 	}
 	
 	@IBAction func desiredLogChanged(_ sender: AnyObject?) {
-		guard let newType = LogType(rawValue: logPopup.selectedTag()) else { fatalError("change to unknown log type") }
-		guard let storage = logView.textStorage else { fatalError("no text storage for log view") }
-		selectedLog = newType
-		storage.replaceCharacters(in: storage.string.fullNSRange, with: logs[selectedLog.rawValue])
+//		guard let newType = LogType(rawValue: logPopup.selectedTag()) else { fatalError("change to unknown log type") }
+//		guard let storage = logView.textStorage else { fatalError("no text storage for log view") }
+//		selectedLog = newType
+//		storage.replaceCharacters(in: storage.string.fullNSRange, with: logs[selectedLog.rawValue])
 		logView.scrollToEndOfDocument(nil)
 	}
 }
