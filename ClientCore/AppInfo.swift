3c81529d1e71e62ce77cef6223e8ea0bd269ef84
Mark Lilback <mark@lilback.com>
2017-11-28 13:51:14 -0500

    added function to tell if app is being debugged

--- ClientCore/AppInfo.swift
+++ ClientCore/AppInfo.swift
@@ -29,4 +29,14 @@ public struct AppInfo {
 		}
 		return subdir
 	}
+	
+	/// Checks if the app is running in a debugger
+	public static var amIBeingDebugged: Bool = {
+		var info = kinfo_proc()
+		var mib : [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
+		var size = MemoryLayout<kinfo_proc>.stride
+		let junk = sysctl(&mib, UInt32(mib.count), &info, &size, nil, 0)
+		assert(junk == 0, "sysctl failed")
+		return (info.kp_proc.p_flag & P_TRACED) != 0
+	}()
 }
