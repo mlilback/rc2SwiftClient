b81c7a092b3bf09fe714a14fae8048441efbb616
Mark Lilback <mark@lilback.com>
2018-01-28 10:35:02 -0500

    implemented more connectionInfo unit tests

--- NetworkingTests/ConnectionInfoSpec.swift
+++ NetworkingTests/ConnectionInfoSpec.swift
@@ -50,32 +50,75 @@ class ConnectionInfoSpec: NetworkingBaseSpec {
 			}
 			
 			it("add workspace") {
-				// TODO: implement
+				expect(project100.workspace(withId: 211)).to(beNil())
+				let newWspace = Workspace(id: 211, version: 1, name: "added", userId: 1, projectId: 100, uniqueId: "123432234234", lastAccess: Date(), dateCreated: Date())
+				var wspacesDict = conInfo.bulkInfo.workspaces
+				var wspaces = wspacesDict[100]!
+				wspaces.append(newWspace)
+				wspacesDict[100] = wspaces
+				let updatedBulk = BulkUserInfo(user: conInfo.bulkInfo.user, projects: conInfo.bulkInfo.projects, workspaces: wspacesDict, files: conInfo.bulkInfo.files)
+				conInfo.update(bulkInfo: updatedBulk)
+				expect(project100.workspaces.value.count).to(equal(wspaces.count))
+				expect(project100.workspace(withId: newWspace.id)?.model).to(equal(newWspace))
 			}
 
-			it("fail to remove last workspace") {
-				// TODO: implement
-			}
-			
 			it("remove file") {
-				// TODO: implement
+				var rawFiles = conInfo.bulkInfo.files[wspace100.wspaceId]!
+				let originalCount = rawFiles.count
+				let removedFileId = rawFiles.last!.id
+				rawFiles.removeLast()
+				var fileDict = conInfo.bulkInfo.files
+				fileDict[wspace100.wspaceId] = rawFiles
+				let updatedBulk = BulkUserInfo(user: conInfo.bulkInfo.user, projects: conInfo.bulkInfo.projects, workspaces: conInfo.bulkInfo.workspaces, files: fileDict)
+				conInfo.update(bulkInfo: updatedBulk)
+				expect(wspace100.file(withId: removedFileId)).to(beNil())
+				expect(wspace100.files.count).to(equal(originalCount - 1))
 			}
 			
 			it("add and update file") {
-				// TODO: implement
+				let newFile = File(id: 455, wspaceId: wspace100.wspaceId, name: "foobar.R", version: 2, dateCreated: Date(), lastModified: Date(), fileSize: 1201)
+				var rawFiles = conInfo.bulkInfo.files[wspace100.wspaceId]!
+				rawFiles.append(newFile)
+				let updatingFile = wspace100.files.last!
+				let updatedFile = File(id: updatingFile.fileId, wspaceId: updatingFile.wspaceId, name: "updatedFile.R", version: updatingFile.version + 1, dateCreated: updatingFile.dateCreated, lastModified: updatingFile.lastModified, fileSize: updatingFile.fileSize)
+				let updateIndex = rawFiles.index(where: { $0.id == updatingFile.fileId })!
+				rawFiles[updateIndex] = updatedFile
+				var fileDict = conInfo.bulkInfo.files
+				fileDict[wspace100.wspaceId] = rawFiles
+				let updatedBulk = BulkUserInfo(user: conInfo.bulkInfo.user, projects: conInfo.bulkInfo.projects, workspaces: conInfo.bulkInfo.workspaces, files: fileDict)
+				conInfo.update(bulkInfo: updatedBulk)
+				// verify added file
+				expect(wspace100.files.count).to(equal(rawFiles.count))
+				expect(wspace100.file(withId: newFile.id)?.model).to(equal(newFile))
+				// verify updated file
+				expect(wspace100.file(withId: updatedFile.id)?.name).to(equal(updatedFile.name))
 			}
 			
-			it("add project") {
-				// TODO: implement
+			it("add and remove project") {
+				let newProject = Project(id: 432, version: 1, userId: 100, name: "dummyproj")
+				let newWspace = Workspace(id: 6534, version: 1, name: "newws", userId: 100, projectId: newProject.id, uniqueId: "sgdfsgsfasdfdsfsf", lastAccess: Date(), dateCreated: Date())
+				let newFile1 = File(id: 542, wspaceId: newWspace.id, name: "file1.txt", version: 1, dateCreated: Date(), lastModified: Date(), fileSize: 121)
+				let newFile2 = File(id: 1542, wspaceId: newWspace.id, name: "file2.Rmd", version: 1, dateCreated: Date(), lastModified: Date(), fileSize: 3121)
+				var wspaces = conInfo.bulkInfo.workspaces
+				wspaces[newProject.id] = [newWspace]
+				var files = conInfo.bulkInfo.files
+				files[newWspace.id] = [newFile1, newFile2]
+				let updatedBulk = BulkUserInfo(user: conInfo.bulkInfo.user, projects: conInfo.bulkInfo.projects + [newProject], workspaces: wspaces, files: files)
+				conInfo.update(bulkInfo: updatedBulk)
+				let addedProj = try! conInfo.project(withId: newProject.id)
+				expect(addedProj.model).to(equal(newProject))
+				let addedWspace = addedProj.workspaces.value.first!
+				expect(addedWspace.model).to(equal(newWspace))
+				expect(addedWspace.files.count).to(equal(2))
+				expect(addedWspace.file(withId: newFile1.id)?.model).to(equal(newFile1))
+				expect(addedWspace.file(withId: newFile2.id)?.model).to(equal(newFile2))
+				
+				//remove the project
+				let originalBulk: BulkUserInfo = try! conInfo.decode(data: bulkData)
+				conInfo.update(bulkInfo: originalBulk)
+				expect { try conInfo.project(withId: newProject.id) }.to(throwError())
 			}
 			
-			it("remove project") {
-				// TODO: implement
-			}
-			
-			it("fail to remove last project") {
-				// TODO: implement
-			}
 		}
 	}
 }
