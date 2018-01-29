a6cd30a322ccdb90aa109f2ff91580f8497a6890
Mark Lilback <mark@lilback.com>
2018-01-29 09:39:43 -0500

    FileCache no longer forces results on main queue. Caller should specify queue via .on(queue:)

--- Networking/FileCache.swift
+++ Networking/FileCache.swift
@@ -567,11 +567,9 @@ extension DefaultFileCache: URLSessionDownloadDelegate {
 					return
 				}
 			}
-			DispatchQueue.main.async {
 				dloadTask.observer.send(value: 1.0)
 				dloadTask.observer.sendCompleted()
 				Log.info("successfully downloaded file \(dloadTask.file.fileId)", .cache)
-			}
 		}
 	}
 	
