9434c2a32f7b384a63b56d3cab62831923de559c
Mark Lilback <mark@lilback.com>
2018-02-10 17:09:04 -0500

    extracted DimmingView to its own source file

--- MacClient/session/RootViewController.swift
+++ MacClient/session/RootViewController.swift
@@ -355,36 +355,6 @@ extension RootViewController: FileViewControllerDelegate {
 	}
 }
 
-// MARK: -
-class DimmingView: NSView {
-	override required init(frame frameRect: NSRect) {
-		super.init(frame: frameRect)
-	}
-
-	required init?(coder: NSCoder) {
-		fatalError("DimmingView does not support NSCoding")
-	}
-
-	override func viewDidMoveToSuperview() {
-		guard let view = superview else { return }
-		translatesAutoresizingMaskIntoConstraints = false
-		wantsLayer = true
-		layer!.backgroundColor = NSColor.black.withAlphaComponent(0.1).cgColor
-		view.addConstraint(leadingAnchor.constraint(equalTo: view.leadingAnchor))
-		view.addConstraint(trailingAnchor.constraint(equalTo: view.trailingAnchor))
-		view.addConstraint(topAnchor.constraint(equalTo: view.topAnchor))
-		view.addConstraint(bottomAnchor.constraint(equalTo: view.bottomAnchor))
-		isHidden = true
-	}
-
-	override func hitTest(_ aPoint: NSPoint) -> NSView? {
-		if !isHidden {
-			return self
-		}
-		return super.hitTest(aPoint)
-	}
-}
-
 class RootView: NSView {
 	var dimmingView: DimmingView?
 	//if dimmingView is visible, block all subviews from getting clicks
---
+++ MacClient/views/DimmingView.swift
@@ -0,0 +1,37 @@
+//
+//  DimmingView.swift
+//
+//  Copyright ©2017 Mark Lilback. This file is licensed under the ISC license.
+//
+
+import Cocoa
+
+class DimmingView: NSView {
+	override required init(frame frameRect: NSRect) {
+		super.init(frame: frameRect)
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("DimmingView does not support NSCoding")
+	}
+	
+	override func viewDidMoveToSuperview() {
+		guard let view = superview else { return }
+		translatesAutoresizingMaskIntoConstraints = false
+		wantsLayer = true
+		layer!.backgroundColor = NSColor.black.withAlphaComponent(0.1).cgColor
+		view.addConstraint(leadingAnchor.constraint(equalTo: view.leadingAnchor))
+		view.addConstraint(trailingAnchor.constraint(equalTo: view.trailingAnchor))
+		view.addConstraint(topAnchor.constraint(equalTo: view.topAnchor))
+		view.addConstraint(bottomAnchor.constraint(equalTo: view.bottomAnchor))
+		isHidden = true
+	}
+	
+	override func hitTest(_ aPoint: NSPoint) -> NSView? {
+		if !isHidden {
+			return self
+		}
+		return super.hitTest(aPoint)
+	}
+}
+
