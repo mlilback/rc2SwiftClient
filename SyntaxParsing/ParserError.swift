db4df54527a97b7fabbcd81a6b36477c1cc769da
Mark Lilback <mark@lilback.com>
2018-03-16 19:40:21 -0400

    adding a parsed document object, parser error enum

---
+++ SyntaxParsing/ParserError.swift
@@ -0,0 +1,11 @@
+//
+//  ParserError.swift
+//
+//  Copyright ©2017 Mark Lilback. This file is licensed under the ISC license.
+//
+
+import Foundation
+
+public enum ParserError: Error {
+	case failedToParse
+}
