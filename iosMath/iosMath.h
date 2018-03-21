63f9bfcc60a5c256bcc54eb3ce6ec24d8a0b4da1
Mark Lilback <mark@lilback.com>
2018-03-21 07:44:16 -0400

    added iosMath framework for latex equations. Had to manually make targets because it normally requires cocoa pods.

---
+++ iosMath/iosMath.h
@@ -0,0 +1,26 @@
+//
+//  iosMath.h
+//  iosMath
+//
+//  Created by Mark Lilback on 3/21/18.
+//  Copyright © 2018 Rc2. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for iosMath.
+FOUNDATION_EXPORT double iosMathVersionNumber;
+
+//! Project version string for iosMath.
+FOUNDATION_EXPORT const unsigned char iosMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iosMath/PublicHeader.h>
+
+#import <IosMath/MTMathUILabel.h>
+#import <IosMath/MTMathListDisplay.h>
+#import <IosMath/MTMathList.h>
+#import <IosMath/MTMathListBuilder.h>
+#import <IosMath/MTUnicode.h>
+#import <IosMath/MTFontManager.h>
+#import <IosMath/MTMathAtomFactory.h>
+#import <IosMath/MTMathListIndex.h>
