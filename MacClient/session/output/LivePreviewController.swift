507acd16a7d52ace54b1dfa812ca4126df4aa4dc
Mark Lilback <mark@lilback.com>
2018-11-06 15:12:08 -0500

    changed LiveOutput to use WKWebView instead of NSTextView

--- MacClient/session/output/LivePreviewController.swift
+++ MacClient/session/output/LivePreviewController.swift
@@ -7,14 +7,48 @@
 //
 
 import Cocoa
+import WebKit
+import ClientCore
+import ReactiveSwift
+import Down
+
+protocol LiveContentSource {
+	var context: Property<EditorContext?> { get }
+}
 
 class LivePreviewController: AbstractSessionViewController, OutputController {
 	var contextualMenuDelegate: ContextualMenuDelegate?
+	var context: MutableProperty<EditorContext?> = MutableProperty<EditorContext?>(nil) { didSet { contextChanged() }}
+	private var contextDisposable: Disposable? = nil
+	private var contentsDisposable: Disposable? = nil
 	
-	@IBOutlet weak var outputView: NSTextView!
+	@IBOutlet weak var outputView: WKWebView!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		outputView.string = "Some text here\nJust to see\n"
+		outputView.loadHTMLString("Some <i>text</i> here\n<br>Just to see\n", baseURL: nil)
+	}
+	
+	private func contextChanged() {
+		contentsDisposable?.dispose()
+		contentsDisposable = nil
+		contextDisposable?.dispose()
+		contextDisposable = context.signal.observeResult { [weak self] result in
+			guard let me = self else { return }
+			me.parseContent()
+		}
+		contentsDisposable = context.value?.currentDocument.value?.editedContents.signal.observeValues { [weak self] _ in
+			guard let me = self else { return }
+			me.parseContent()
+		}
+	}
+	
+	private func parseContent() {
+		guard let newText = context.value?.currentDocument.value?.currentContents else {
+			outputView.loadHTMLString("", baseURL: nil)
+			return
+		}
+		// TODO: send file directory as baseURL
+		outputView.loadHTMLString(newText, baseURL: nil)
 	}
 }
