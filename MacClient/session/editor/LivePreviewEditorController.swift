200386347b0186578494e45ee3a0052081fa5eb0
Mark Lilback <mark@lilback.com>
2019-06-13 14:53:41 -0400

    preview editor now has basic capabilities from source editor

--- MacClient/session/editor/LivePreviewEditorController.swift
+++ MacClient/session/editor/LivePreviewEditorController.swift
@@ -10,22 +10,21 @@ import Cocoa
 import Down
 import MJLLogger
 
-class LivePreviewEditorController: AbstractEditorController {
-	@IBOutlet weak var textEditor: SessionEditor!
+class LivePreviewEditorController: BaseSourceEditorController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		textEditor.isEditable = true
+		editor?.isEditable = true
 	}
 
-	override func loaded(content: String) {
-		do {
-			let rendered = try content.toHTML([.normalize, .safe, .sourcePos])
-			textEditor?.string = rendered
-		} catch {
-			Log.warn("preview editor failed to parse markdown")
-		}
-	}
+//	override func loaded(content: String) {
+//		do {
+//			let rendered = try content.toHTML([.normalize, .safe, .sourcePos])
+//			editor?.string = rendered
+//		} catch {
+//			Log.warn("preview editor failed to parse markdown")
+//		}
+//	}
 	
 	/// subclasses should override and save contents via save(edits:). super should not be called
 	override func editsNeedSaving() {
