f1c9df7931661dd24ef04b91d3fabfa5da7e7b6f
Mark Lilback <mark@lilback.com>
2019-08-04 14:08:51 -0400

    updated preview so output gets proper notification when it needs to update preview

--- MacClient/session/editor/LivePreviewEditorController.swift
+++ MacClient/session/editor/LivePreviewEditorController.swift
@@ -12,21 +12,57 @@ import MJLLogger
 import ClientCore
 import WebKit
 
+let typingDelayBeforeUpdatingPreview: TimeInterval = 0.4
+
 class LivePreviewEditorController: BaseSourceEditorController {
 	var outputController: LivePreviewOutputController? { didSet { outputControllerChanged() } }
 	
 	private var webView: WKWebView?
+	private var inChange = false
+	private var lastTextChange: TimeInterval = 0
+	private let lastChangeQueue = DispatchQueue(label: "preview text change")
+	private var lastChangeTimer: DispatchSourceTimer!
+	private var lastChangeRange : NSRange?
+	private var lastChangeDelta: Int = 0
+	private var timerRunning = false
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		editor?.isEditable = true
+		lastChangeTimer = DispatchSource.makeTimerSource(queue: .main)
+		lastChangeTimer.schedule(deadline: .now() + .milliseconds(500), repeating: .milliseconds(500), leeway: .milliseconds(100))
+		lastChangeTimer.setEventHandler {
+			self.lastChangeQueue.sync {
+				guard let lastRange = self.lastChangeRange else { return }
+				let curTime = Date.timeIntervalSinceReferenceDate
+				if (curTime - self.lastTextChange) > typingDelayBeforeUpdatingPreview {
+					self.lastTextChange = curTime
+					defer { self.lastChangeRange = nil }
+					if let oc = self.outputController,
+						oc.contentsEdited(range: lastRange, delta: self.lastChangeDelta),
+						let newContents = self.editor?.string
+					{
+						self.save(edits: newContents)
+					}
+				}
+			}
+		}
+//		lastChangeQueue.sync {
+			lastChangeTimer.activate()
+			lastChangeTimer.suspend()
+//		}
 	}
 
 	override func setContext(context: EditorContext) {
 		super.setContext(context: context)
+		outputController?.editorContext = context
 	}
 	
+	// outputControllerChanged was being called recursively. Use inChange flag to prevent that
 	func outputControllerChanged() {
+		guard !inChange else { return }
+		inChange = true
+		defer { inChange = false }
 		outputController?.editorContext = self.context
 		// need to update with current generated contents
 	}
@@ -40,8 +76,27 @@ class LivePreviewEditorController: BaseSourceEditorController {
 //		}
 //	}
 	
+	override func contentsChanged(_ contents: NSTextStorage, range: NSRange, changeLength delta: Int) {
+		super.contentsChanged(contents, range: range, changeLength: delta)
+		lastChangeQueue.sync {
+			lastTextChange = Date.timeIntervalSinceReferenceDate
+			lastChangeRange = range
+			lastChangeDelta = delta
+		}
+	}
+	
 	/// subclasses should override and save contents via save(edits:). super should not be called
 	override func editsNeedSaving() {
 		
+	}
+	
+	func textDidBeginEditing(_ notification: Notification) {
+		guard !timerRunning else { return }
+		lastChangeTimer.resume()
+	}
+	
+	func textDidEndEditing(_ notification: Notification) {
+		lastChangeTimer.suspend()
+		timerRunning = false
 	}
 }
--- MacClient/session/output/LivePreviewDisplayController.swift
+++ MacClient/session/output/LivePreviewDisplayController.swift
@@ -9,6 +9,7 @@
 import Cocoa
 import WebKit
 import ClientCore
+import SyntaxParsing
 import ReactiveSwift
 import Down
 
@@ -17,49 +18,112 @@ protocol LivePreviewOutputController {
 //	var webView: WKWebView { get }
 	/// 
 //	var sessionController: SessionController? { get set }
-	/// alows preview editor to tell display controller what the current context is so it can monitor the current document
+	/// allows preview editor to tell display controller what the current context is so it can monitor the current document
 	var editorContext: EditorContext? { get set }
+	/// allows editor that the user has made a change to the contents of the current document
+	///
+	/// - Parameters:
+	///   - range: the range of the text in the original string
+	///   - delta: the change made
+	/// - Returns: true if the changes should be saved to the current document's editedContents causing a reparse
+	func contentsEdited(range: NSRange, delta: Int) -> Bool
 }
 
 class LivePreviewDisplayController: AbstractSessionViewController, OutputController, LivePreviewOutputController {
 	var contextualMenuDelegate: ContextualMenuDelegate?
 	var sessionController: SessionController?
 	private var context: MutableProperty<EditorContext?> = MutableProperty<EditorContext?>(nil) { didSet { contextChanged() }}
-	private var contextDisposable: Disposable? = nil
-	private var contentsDisposable: Disposable? = nil
+	private let contextDisposables = CompositeDisposable()
+	private var contentsDisposable: Disposable?
 	
 	var editorContext: EditorContext? {
 		get { return context.value }
-		set { context.value = newValue }
+		set {
+			context.value = newValue
+			contextChanged()
+		}
 	}
 	
-	@IBOutlet weak var outputView: WKWebView!
+	@IBOutlet weak var outputView: WKWebView?
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		outputView.loadHTMLString("Some <i>text</i> here\n<br>Just to see\n", baseURL: nil)
+		do {
+			guard let headerUrl = Bundle.main.url(forResource: "customRmdHeader", withExtension: "html"),
+				let footerUrl = Bundle.main.url(forResource: "customRmdFooter", withExtension: "html")
+			else {
+				fatalError("failed to load live preview template file")
+			}
+			let header = try String(contentsOf: headerUrl)
+			let footer = try String(contentsOf: footerUrl)
+			outputView?.loadHTMLString("\(header) Body <span id\"foobar\">Goes</span> Here \(footer)", baseURL: nil)
+		} catch {
+			fatalError("failed to load live preview template file: \(error.localizedDescription)")
+		}
+	}
+	
+	/// called by the editor when the user has made an edit, resulting in a change of what is displayed
+	///
+	/// - Parameters:
+	///   - range: the range of the text in the original string
+	///   - delta: the change made
+	func contentsEdited(range: NSRange, delta: Int) -> Bool {
+		/// FIXME: implement
+		// need to update the changed chunk if possible. If not, then reparse document
+		return false
 	}
 	
 	private func contextChanged() {
+		contextDisposables.dispose()
 		contentsDisposable?.dispose()
 		contentsDisposable = nil
-		contextDisposable?.dispose()
-		contextDisposable = context.signal.observeResult { [weak self] result in
-			guard let me = self else { return }
-			me.parseContent()
+		// need to listen for currentDocument changes, then observe content changes of that document
+		contextDisposables += context.signal.observeValues { [weak self] _ in
+			guard let me = self, let context = me.context.value else { return }
+			me.contextDisposables += context.parsedDocument.signal.observeValues { [weak self] _ in
+				guard let me = self else { return }
+				// document changed, re-observe contents
+				me.documentChanged()
+			}
+			me.documentChanged()
 		}
-		contentsDisposable = context.value?.currentDocument.value?.editedContents.signal.observeValues { [weak self] _ in
+		documentChanged()
+	}
+	
+	private func documentChanged() {
+		contentsDisposable?.dispose()
+		contentsDisposable = nil
+		guard let context = editorContext else { return }
+		contentsDisposable = context.parsedDocument.signal.observeValues { [weak self] _ in
 			guard let me = self else { return }
 			me.parseContent()
 		}
 	}
 	
 	private func parseContent() {
-		guard let newText = context.value?.currentDocument.value?.currentContents else {
-			outputView.loadHTMLString("", baseURL: nil)
+		guard let curDoc = context.value?.currentDocument.value,
+			let newText = curDoc.currentContents
+		else {
+			outputView?.loadHTMLString("", baseURL: nil)
 			return
 		}
+		let content = """
+			document.getElementById('foobar').innerText = <b>blow me</b>;
+		"""
+		outputView?.evaluateJavaScript(content, completionHandler: nil)
 		// TODO: send file directory as baseURL
-		outputView.loadHTMLString(newText, baseURL: nil)
+		//outputView.loadHTMLString(newText, baseURL: nil)
+	}
+	
+	private func escapeForJavascript(_ source: String) -> String {
+		var string = source
+		string = string.replacingOccurrences(of: "\\", with: "\\\\")
+		string = string.replacingOccurrences(of: "\"", with: "\\\"")
+		string = string.replacingOccurrences(of: "\'", with: "\\\'")
+		string = string.replacingOccurrences(of: "\n", with: "\\n")
+		string = string.replacingOccurrences(of: "\r", with: "\\r")
+		// swift does not recognize this as a valid sequence needing escape
+		// string = string.replacingOccurrences(of: "\f", with: "\\f")
+		return string
 	}
 }
