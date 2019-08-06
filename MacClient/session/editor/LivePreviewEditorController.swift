0fcada546e84c0a53553d9f285e8476c66310e6b
Mark Lilback <mark@lilback.com>
2019-08-06 12:54:41 -0400

    renamed inChange property to inOutputChange. Removed lastChangeQueue because all work should be done on main thread

--- MacClient/session/editor/LivePreviewEditorController.swift
+++ MacClient/session/editor/LivePreviewEditorController.swift
@@ -18,9 +18,12 @@ class LivePreviewEditorController: BaseSourceEditorController {
 	var outputController: LivePreviewOutputController? { didSet { outputControllerChanged() } }
 	
 	private var webView: WKWebView?
-	private var inChange = false
+	
+	// used to prevent recursion (due to didSet) in outputControllerChanged()
+	private var inOutputChange = false
+
+	// used to wrap lastChange with a queue, but that was causing recursive errors. Instead, this values should only be used on the main thread
 	private var lastTextChange: TimeInterval = 0
-	private let lastChangeQueue = DispatchQueue(label: "preview text change")
 	private var lastChangeTimer: DispatchSourceTimer!
 	private var lastChangeRange : NSRange?
 	private var lastChangeDelta: Int = 0
@@ -32,25 +35,24 @@ class LivePreviewEditorController: BaseSourceEditorController {
 		lastChangeTimer = DispatchSource.makeTimerSource(queue: .main)
 		lastChangeTimer.schedule(deadline: .now() + .milliseconds(500), repeating: .milliseconds(500), leeway: .milliseconds(100))
 		lastChangeTimer.setEventHandler {
-			self.lastChangeQueue.sync {
-				guard let lastRange = self.lastChangeRange else { return }
-				let curTime = Date.timeIntervalSinceReferenceDate
-				if (curTime - self.lastTextChange) > typingDelayBeforeUpdatingPreview {
-					self.lastTextChange = curTime
-					defer { self.lastChangeRange = nil }
-					if let oc = self.outputController,
-						oc.contentsEdited(range: lastRange, delta: self.lastChangeDelta),
-						let newContents = self.editor?.string
-					{
-						self.save(edits: newContents)
-					}
+			guard let lastRange = self.lastChangeRange else { return }
+			let curTime = Date.timeIntervalSinceReferenceDate
+			if (curTime - self.lastTextChange) > typingDelayBeforeUpdatingPreview {
+				self.lastTextChange = curTime
+				defer { self.lastChangeRange = nil }
+				if let oc = self.outputController,
+					oc.contentsEdited(range: lastRange, delta: self.lastChangeDelta),
+					let newContents = self.editor?.string
+				{
+					if self.ignoreContentChanges { return }
+						self.save(edits: newContents, reload: true)
+						// FIXME: should be updating the current chunk, or the entire document
+						self.context?.parseCurrentDocument()
 				}
 			}
 		}
-//		lastChangeQueue.sync {
-			lastChangeTimer.activate()
-			lastChangeTimer.suspend()
-//		}
+		lastChangeTimer.activate()
+		lastChangeTimer.suspend()
 	}
 
 	override func setContext(context: EditorContext) {
@@ -60,9 +62,9 @@ class LivePreviewEditorController: BaseSourceEditorController {
 	
 	// outputControllerChanged was being called recursively. Use inChange flag to prevent that
 	func outputControllerChanged() {
-		guard !inChange else { return }
-		inChange = true
-		defer { inChange = false }
+		guard !inOutputChange else { return }
+		inOutputChange = true
+		defer { inOutputChange = false }
 		outputController?.editorContext = self.context
 		// need to update with current generated contents
 	}
@@ -78,11 +80,9 @@ class LivePreviewEditorController: BaseSourceEditorController {
 	
 	override func contentsChanged(_ contents: NSTextStorage, range: NSRange, changeLength delta: Int) {
 		super.contentsChanged(contents, range: range, changeLength: delta)
-		lastChangeQueue.sync {
-			lastTextChange = Date.timeIntervalSinceReferenceDate
-			lastChangeRange = range
-			lastChangeDelta = delta
-		}
+		lastTextChange = Date.timeIntervalSinceReferenceDate
+		lastChangeRange = range
+		lastChangeDelta = delta
 	}
 	
 	/// subclasses should override and save contents via save(edits:). super should not be called
