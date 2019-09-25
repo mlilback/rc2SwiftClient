5d533013d3187e8f2e704db216c72c07c1d0148c
Mark Lilback <mark@lilback.com>
2019-09-25 10:06:07 -0400

    open session now longer automatically shows trace of reactive state

--- MacClient/MacAppDelegate.swift
+++ MacClient/MacAppDelegate.swift
@@ -220,7 +220,7 @@ extension MacAppDelegate {
 		guard let conInfo = connectionManager.currentConnection
 			else { Log.warn("asked to open session without connection info"); return }
 		let session = Session(connectionInfo: conInfo, workspace: workspace)
-		session.open().observe(on: UIScheduler()).take(during: session.lifetime).logEvents(identifier: "open session").start
+		session.open().observe(on: UIScheduler()).take(during: session.lifetime).optionalLog("open session").start
 			{ [weak self] event in
 				switch event {
 				case .completed:
